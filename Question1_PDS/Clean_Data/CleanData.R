4b5f06bd093b73893579877c84ca6742c2453f27
nikitha2482 <159867743+nikitha2482@users.noreply.github.com>
2024-02-13 09:10:37 -0600

    Update CleanData.R

--- Question1_PDS/Clean_Data/CleanData.R
+++ Question1_PDS/Clean_Data/CleanData.R
@@ -1,4 +1,4 @@
-raw_yield_data <- read.csv("C:/Users/DELL/OneDrive/Desktop/SEM1 UMKC/SEM3/PDS/PDS DATASET.csv")
+raw_yield_data <- read.csv("C:\Users\MyPC\Downloads\StudentsPerformance.csv")
 View (raw_yield_data)
 #attach(raw_yield_data)
 cleaned_yield_data <- na.omit (raw_yield_data[raw_yield_data$Frailty !="NA",])
